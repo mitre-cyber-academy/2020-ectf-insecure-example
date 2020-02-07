d7071b844f74960f6abbdb80dea385fc1fe2ae93
Drew Monroe <dmonroe@mitre.org>
2020-02-07 14:37:43 -0500

    Add various clarifications and bugfixes
    - Update output location of `miPod` application
    - Update bitstream output location to be in the device directory
    - Ensure that the correct bitstream is used when passing to updatemem
    - Remove .h from device secrets
    - Add digital_out fix

--- tools/combine_bitstream.tcl
+++ tools/combine_bitstream.tcl
@@ -5,20 +5,21 @@
 #   /media/sf_Vagrant/2020-ectf/
 #******************************************************************
 
-if { $argc != 2 } {
+if { $argc != 3 } {
     puts "buildMicroblaze requires the user to provide the following:
     dev_path and proj_name."
     exit 1
 }
 
 set dev_path [lindex $argv 0]
-set proj_name [lindex $argv 1]
+set device_dir [lindex $argv 1]
+set proj_name [lindex $argv 2]
 set worksp "$dev_path/mb"
 
-set sw_mmi "$worksp/Cora-Z7-07S/system_wrapper.mmi"
-set sw_bit "$worksp/Cora-Z7-07S/system_wrapper.bit"
+set sw_mmi "$worksp/$proj_name/system_wrapper.mmi"
+set sw_bit "$worksp/$proj_name/system_wrapper.bit"
 set drm_elf "$worksp/drm_audio_fw/Debug/drm_audio_fw.elf"
-set output "$dev_path/pl/proj/$proj_name/download.bit"
+set output "$device_dir/download.bit"
 
 puts "\nCalling updatemem as follows:
 updatemem -force -meminfo \
