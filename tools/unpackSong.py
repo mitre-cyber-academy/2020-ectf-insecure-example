db33582395ea4f1dafd1c829c61adc984219bb29
Justin Campbell <justincampbell@pal-nat186-164-185.itap.purdue.edu>
2020-03-01 14:13:53 -0500

    Delete unpackSong.py

--- tools/unpackSong.py
+++
@@ -1,8 +0,0 @@
-import struct
-import os
-import wave
-
-
-protected_wave = wave.open(os.path.abspath("/home/tthomas/mitre/2020-ectf-insecure-example/sample-audio/demo_flag.drm", 'rb')
-
-unprotected_wave = struct.unpack('<h', 
